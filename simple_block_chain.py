71c9872b97ae2ad94ece12173908cda0ac91744d
Sibin Thomas <54390036+SibinThomasQuad@users.noreply.github.com>
2023-06-01 18:04:38 +0530

    Create simple_block_chain.py

---
+++ simple_block_chain.py
@@ -0,0 +1,56 @@
+import hashlib
+import datetime
+
+class Block:
+    def __init__(self, timestamp, data, previous_hash):
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        hash_string = str(self.timestamp) + str(self.data) + str(self.previous_hash)
+        return hashlib.sha256(hash_string.encode('utf-8')).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block(datetime.datetime.now(), 'Genesis Block', '0')
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+
+            if current_block.hash != current_block.calculate_hash():
+                return False
+
+            if current_block.previous_hash != previous_block.hash:
+                return False
+
+        return True
+
+# Create a blockchain and add some blocks
+my_blockchain = Blockchain()
+my_blockchain.add_block(Block(datetime.datetime.now(), {'amount': 5}, ''))
+my_blockchain.add_block(Block(datetime.datetime.now(), {'amount': 10}, ''))
+my_blockchain.add_block(Block(datetime.datetime.now(), {'amount': 15}, ''))
+
+# Print the blockchain
+print("-------------BLOCK CHAIN----------------")
+for block in my_blockchain.chain:
+    print("Timestamp:", block.timestamp)
+    print("Data:", block.data)
+    print("Hash:", block.hash)
+    print("Previous Hash:", block.previous_hash)
+    print("-----------------------------"*2)
