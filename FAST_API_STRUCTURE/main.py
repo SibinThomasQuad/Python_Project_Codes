f0d35018463db3deab07dbcbc728a4f3cc2d7436
Sibin Thomas <54390036+SibinThomasQuad@users.noreply.github.com>
2023-10-04 15:43:56 +0530

    Create main.py

---
+++ FAST_API_STRUCTURE/main.py
@@ -0,0 +1,14 @@
+my_fastapi_app/
+├── app/
+│   ├── __init__.py
+│   ├── main.py
+│   ├── models.py
+│   └── routers/
+│       ├── __init__.py
+│       └── some_router.py
+├── tests/
+│   ├── __init__.py
+│   └── test_main.py
+├── requirements.txt
+├── README.md
+└── main.py
